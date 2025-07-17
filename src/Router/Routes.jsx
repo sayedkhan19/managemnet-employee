c8d016b44e80bb4a93acde7123a905a56ed75c42
sayedkhan196 <sayedkhan196>
2025-07-17 11:01:44 +0600

    einvironment set

---
+++ src/Router/Routes.jsx
@@ -0,0 +1,10 @@
+import {
+  createBrowserRouter,
+  } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-4xl ">Hello World</div>,
+  },
+]);
